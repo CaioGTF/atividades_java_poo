31a36b75a75f93d23353f16111863e694c73d1ed
Caio Gabriel <105945358+CaioGTF@users.noreply.github.com>
2025-03-29 22:21:50 +0000

    java

---
+++ primeira_lista/oitava_questao/ControleEventos.java
@@ -0,0 +1,14 @@
+
+
+public class ControleEventos {
+    public static void main(String[] args) {
+        Evento e1 = new Evento("Lafolie", "20/05/2025", "Boa Vista" );
+        Evento e1 = new Evento("The eras tour", "22/06/2025", "Madalena");
+        Evento e3 = new Evento("MarvelCompany", "05/05/2026", "New York");
+
+
+        e1.exibirEvento();
+        e2.exibirEvento();
+        e3.exibirEvento();
+    }
+}
---
+++ primeira_lista/oitava_questao/Evento.java
@@ -0,0 +1,39 @@
+public class Evento {
+    private String nomeEvento;
+    private String data;
+    private String local;
+
+    public Evento() {}
+
+    public Evento(String nomeEvento, String data, String local){
+        this.nomeEvento = nomeEvento;
+        this.data = data;
+        this.local = local;
+    }
+
+    public String getNomeEvento(){
+        return nomeEvento;
+    }
+    public void setNomeEvento(String nomeEvento){
+        this.nomeEvento = nomeEvento;
+    }
+    public String getData(){
+        return data;
+    }
+    public void setData(String data){
+        this.data = data;
+    }
+    public String getLocal(){
+        return local;
+    }
+    public void setLocal(String local){
+        this.local = local;
+    }
+
+    public void exibirEvento() {
+        System.out.println("Evento: " + nomeEvento);
+        System.out.println("Data: " + data);
+        System.out.println("Local: " + local);
+        System.out.println("--------------------------");
+    }
+}
